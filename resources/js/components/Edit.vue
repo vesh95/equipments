b87f64347a9b27b8ce4877cf0747960429304379
Eduaed <vesh95.17@ya.ru>
2022-03-24 15:43:27 +0500

    Changed edit equipment component

--- resources/js/components/Edit.vue
+++ resources/js/components/Edit.vue
@@ -7,6 +7,7 @@
                 class="form-control"
                 v-model="formData.serialNumber"
             >
+            <error-attribute :errors="errors.serialNumber" />
         </div>
         <div class="form-group">
             <select class="form-control" v-model="formData.equipmentTypeId">
@@ -17,10 +18,12 @@
                     {{ option.label }}
                 </option>
             </select>
+            <error-attribute :errors="errors.equipmentTypeId" />
         </div>
         <div class="form-group">
             <textarea class="form-control" v-model="formData.note">
             </textarea>
+            <error-attribute :errors="errors.note" />
         </div>
 
         <div class="action-group">
@@ -36,29 +39,40 @@
 <script>
 import store from "../store";
 import router from "../router";
+import ErrorAttribute from "./ErrorAttribute";
 
 export default {
+    components: {
+        ErrorAttribute
+    },
+
     data() {
         return {
-            formData: {}
+            formData: {},
+            errors: {},
         }
     },
     mounted() {
-        this.formData = Object.create(this.$store.state.equipments.find((value) => {
-            return value.id === Number(this.$route.params.id)
-        }))
+        axios.get(`/api/equipment/${this.$route.params.id}`)
+            .then((response) => {
+                this.formData = response.data.data
+            })
+        this.errors = {}
     },
     methods: {
         updateEquipments() {
-            axios.patch(`api/equipment/${this.formData.id}`, {
+            axios.patch(`/api/equipment/${this.formData.id}`, {
                 equipmentTypeId: this.formData.equipmentTypeId,
                 serialNumber: this.formData.serialNumber,
                 note: this.formData.note
             })
-            .then((response) => {
-                store.commit('updateEquipment', response.data.data)
-                router.push('/')
-            })
+                .then((response) => {
+                    store.commit('updateEquipment', response.data.data)
+                    router.push('/')
+                })
+                .catch((error) => {
+                    this.errors = error.response.data
+                })
         }
     },
     computed: {
---
+++ resources/js/components/ErrorAttribute.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="text-danger" v-if="errors?.length" v-for="errorMessage in errors">
+        <li>
+            <small>{{ errorMessage }}</small>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    props: {
+        errors: Array
+    },
+}
+</script>
+<style>
+    ul {
+        margin: 0;
+        padding: 0;
+    }
+</style>
